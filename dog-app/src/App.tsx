f66c90cc9a900b8ec797bad366dcd21611428ac7
Hubert <44268774+Hubert06@users.noreply.github.com>
2021-04-27 00:04:47 -0400

    Create react app

---
+++ dog-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="dog-image-carousel">
+        Dog carousel
+      </div>
+    </div>
+  );
+}
+
+export default App;
